de4a2567240f0bcbb1e10f7ef37215a28883c37c
Samuel <samuel.pisa@tegra.me>
2017-07-02 16:37:06 -0300

    corrige controller

--- src/main/java/br/com/cartola/metrics/controller/AtletaController.java
+++ src/main/java/br/com/cartola/metrics/controller/AtletaController.java
@@ -1,14 +1,9 @@
 package br.com.cartola.metrics.controller;
 
-import br.com.cartola.metrics.model.cartola.AtletasResponse;
-import br.com.cartola.metrics.model.cartola.MercadoResponse;
 import br.com.cartola.metrics.repository.AtletaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.math.BigDecimal;
 
 /**
  * Created by samuel on 30/05/17.
@@ -20,23 +15,7 @@ public class AtletaController {
     private AtletaRepository atletaRepository;
 
     @GetMapping(value = "/")
-    private String getAll(){
-
-        MercadoResponse mercado = new RestTemplate()
-                .getForObject("https://api.cartolafc.globo.com/mercado/status",
-                        MercadoResponse.class);
-
-        if(mercado.getStatus_mercado() == 2
-                && mercado.getRodada_atual() > 10) {
-
-            AtletasResponse atletas = new RestTemplate()
-                    .getForObject("https://api.cartolafc.globo.com/atletas/mercado",
-                            AtletasResponse.class);
-
-            atletaRepository.insert(atletas.getAtletas());
-        }
-
-
-        return "";
+    private String index() {
+        return "cartola top";
     }
 }
