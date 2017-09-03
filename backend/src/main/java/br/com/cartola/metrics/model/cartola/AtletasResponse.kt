4d4f6b36ba9ad86ffd7d44e5d3dc52ea40846106
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-09-03 20:13:19 -0300

    converte classes para kotlin

---
+++ backend/src/main/java/br/com/cartola/metrics/model/cartola/AtletasResponse.kt
@@ -0,0 +1,12 @@
+package br.com.cartola.metrics.model.cartola
+
+import br.com.cartola.metrics.model.Atleta
+
+import java.io.Serializable
+
+/**
+ * Created by samuel on 28/06/17.
+ */
+class AtletasResponse{
+    var atletas: List<Atleta> = ArrayList()
+}
