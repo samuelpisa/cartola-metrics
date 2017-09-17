d22c8ef86279819eadd6398432b4cd8808b20db0
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-09-17 19:47:30 -0300

    adiciona router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import App from './App'
+import router from './routes'
 
 /* eslint-disable no-new */
 Vue.use(VueMaterial)
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
 })
