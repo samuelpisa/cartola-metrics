f8396ef6045ceea5a3a324e4d08839f48dc0ee05
Samuel Pisa <samfrezza@gmail.com>
2017-10-01 15:56:36 -0300

    adiciona pagina home

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,17 +1,70 @@
 <template>
-  <div class="hello">
+  <md-layout md-gutter md-align="center">
+    
+    <md-layout md-flex="40" md-flex-xsmall="100">
+      <md-card>
+          <md-card-header>
+              <div class="md-title">Bem vindo ao Cartola Top</div>
+          </md-card-header>
+          <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+      </md-card>
+    </md-layout>
 
-  </div>
+    <md-layout md-flex="40" md-flex-xsmall="100">
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Ranking de Clubes</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-media md-ratio="16:9" md-medium>
+          <img src="static//img//ranking-card.png">
+        </md-card-media>
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+        </md-card-content>
+      </md-card>
+    </md-layout>
+
+    <md-layout md-flex="40" md-flex-xsmall="100">
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Análise da Rodada</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-media md-ratio="16:9" md-medium>
+          <img src="static//img//ranking-card.png">
+        </md-card-media>
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+        </md-card-content>
+      </md-card>
+    </md-layout>
+
+    <md-layout md-flex="40" md-flex-xsmall="100">
+      <md-card>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Escalação Inteligente</div>
+          </md-card-header-text>
+        </md-card-header>
+        <md-card-media md-ratio="16:9" md-medium>
+          <img src="static//img//ranking-card.png">
+        </md-card-media>
+        <md-card-content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+        </md-card-content>
+      </md-card>
+    </md-layout>
+  </md-layout>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'hello'
 }
 </script>
 
--- frontend/src/components/Sobre.vue
+++ frontend/src/components/Sobre.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-      <p>sobre</p>
+  <md-layout md-align="center" md-gutter>
+    <md-layout md-flex="80" md-flex-xsmall="100">
 
-  </div>
+    <md-card>
+        <md-card-header>
+            <div class="md-title">Title sssgoes here</div>
+            <div class="md-subhead">Subtitle here</div>
+        </md-card-header>
+        <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+        </md-card-content>
+    </md-card>
+
+    </md-layout>
+  </md-layout>
 </template>
 
 <script>
