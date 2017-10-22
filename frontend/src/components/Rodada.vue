0d47f7241547029b4344f66ac092c5fa831ff1af
Samuel Pisa <samfrezza@gmail.com>
2017-10-22 16:56:36 -0200

    novo layout analise rodada

--- frontend/src/components/Rodada.vue
+++ frontend/src/components/Rodada.vue
@@ -1,22 +1,79 @@
 <template>
-      <div>
-      {{analise.rodada_id}}
-
-    <md-card v-for="partida in analise.partidas">
+  <md-layout md-align="center">
+     <md-layout md-flex="50" md-flex-xsmall="100">
+    
+        <h3>Análise da rodada {{analise.rodada_id}}</h3>
+      
+    <md-card md-with-hover v-for="partida in analise.partidas" style="width: 600px">
       <md-card-content>
-        <p :class="partida.seloCasa ? 'clube-selo' : ''"><img :src="partida.clubeCasa.escudos.pequeno"/>{{partida.clubeCasa.nome}}</p>
-        x
-        <p :class="partida.seloCasa ? '' : 'clube-selo'"><img :src="partida.clubeVisitante.escudos.pequeno"/>{{partida.clubeVisitante.nome}}</p>
+        <md-layout md-gutter>
+          <md-layout>
+            <md-avatar style="border-radius: 0">
+              <img :src="partida.clubeCasa.escudos.medio"/>
+            </md-avatar>
+            <span>{{partida.clubeCasa.nome}}</span>
+          </md-layout>
+          <md-layout md-align="center">
+            <div>
+              <p>X</p>
+            </div>
+          </md-layout>
+          <md-layout md-align="center">
+             <span>{{partida.clubeVisitante.nome}}</span>
+            <md-avatar style="border-radius: 0">
+              <img :src="partida.clubeVisitante.escudos.medio"/>
+            </md-avatar>
+          </md-layout>
+        </md-layout>
+        <div class="tcenter">
+          <p>{{partida.local}} - {{partida.data | formatDate}}</p>
+        </div>
+        <md-layout>
+          <md-layout>
+             <md-list>
+              <md-list-item>
+                <p>
+                  Pontos: {{partida.clubeCasa.pontos.mediaPontos | formatNumber}}<br>
+                  Pontos Casa: {{partida.clubeCasa.mandante.mediaPontos | formatNumber}}<br>
+                </p>
+              </md-list-item>
+              <md-list-item>
+                <md-progress class="vantagem-bar" :md-progress="partida.vantagemCasa"></md-progress>
+              </md-list-item>
+            </md-list>
+          </md-layout>
 
-        <p>Local: {{partida.local}}</p>
-        <p>valida: {{partida.valida}}</p>
+          <md-layout>
+            <md-list>
+              <md-list-item>
+                <p>
+                  Pontos: {{partida.clubeVisitante.pontos.mediaPontos | formatNumber}}<br>
+                  Pontos Fora: {{partida.clubeVisitante.visitante.mediaPontos | formatNumber}}<br>
+                </p>
+              </md-list-item>
+              <md-list-item>
+                <md-progress class="vantagem-bar" :md-progress="partida.vantagemVisitante"></md-progress>
+              </md-list-item>
+            </md-list>
+          </md-layout>
+        </md-layout>
       </md-card-content>
     </md-card>
-      </div>
+     </md-layout>
+  </md-layout>
 </template>
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
+import Vue from 'vue'
+
+Vue.filter('formatDate', function (value) {
+  moment.locale('pt-br')
+  if (value) {
+    return moment(value).format('DD/MM/YYYY HH:mm')
+  }
+})
 
 export default {
   name: 'rodada',
@@ -42,4 +99,12 @@ export default {
   .clube-selo {
     background-color: #24ab19;
   }
+  .vantagem-bar {
+    background-color: #e5e5e5 !important;
+    height: 10px;
+  }
+  .tcenter {
+    text-align: center;
+  }
+
 </style>
