a830012e9d31409114ecae07d91f91d941fa4fc5
Samuel Frezza Pisa <samfrezza@gmail.com>
2018-04-17 22:23:10 -0300

    corrige deploy

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 mvn clean package
 
 echo "Uploading files"
-scp backend/target/metrics-0.0.1-SNAPSHOT.jar root@138.197.114.203:/var/metrics/metrics-0.0.1-SNAPSHOT.jar
+scp backend/target/metrics-0.0.1-SNAPSHOT.jar root@cartola.top:/var/metrics/metrics-0.0.1-SNAPSHOT.jar
 
 echo "Reloading service"
-ssh root@138.197.114.203 "systemctl daemon-reload"
+ssh root@cartola.top "systemctl daemon-reload"
 
 echo "Restarting application"
-ssh root@138.197.114.203 "systemctl restart metrics"
+ssh root@cartola.top "systemctl restart metrics"
 
 echo "Done"
